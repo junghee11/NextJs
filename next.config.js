699c87f1d29d1c16e0e0bf683f66248b27f19d22
junghee11 <namjunghee11@naver.com>
2026-04-23 13:51:34 +0900

    [develop] docker 환경 구성 (dev/prod)

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+
+  telemetry: false,
+
+  images: {
+    domains: [], 
+  },
+};
+
+module.exports = nextConfig;
